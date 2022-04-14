44c7ad813b9223e180ce0570c0028659872b750d
kennethgargan <kennethgargan@gmail.com>
2022-04-14 11:53:52 +0100

    Added debugging for environmental data & functions for both fan & data getting

--- model.py
+++ model.py
@@ -25,13 +25,25 @@ class Model:
             "vact" : "",
             "sltm" : ""
         }
-
+    
     def update_fan_data(self,payload):
         for key,value in payload.items():
             if key in self.fan_data:
                 self.fan_data[key] = value
+        self.get_fan_data()
 
-    def update_env_data(self,payload: dict) -> None:
+    def update_env_data(self,payload):
         for key,value in payload.items():
             if key in self.env_data:
-                self.env_data[key] == value
+                self.env_data[key] = value
+        self.get_env_data()
+
+    def get_fan_data(self):
+        print("---------")
+        for key,value in self.fan_data.items():
+            print(f"{key} : {value}")
+
+    def get_env_data(self):
+        print("---------")
+        for key,value in self.env_data.items():
+            print(f"{key} : {value}")
